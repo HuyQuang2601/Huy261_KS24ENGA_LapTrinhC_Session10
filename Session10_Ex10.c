b00cb1930fe222797d1434657d1f092a09776170
HuyQuang2601 <huyxauzai2601@gmail.com>
2024-12-03 11:14:59 +0700

    Add files via upload

--- Session10_Ex10.c
+++ Session10_Ex10.c
@@ -2,35 +2,32 @@
 
 int main(){
 	int arr[100][100];
-	int m,n;
+	int m,n,i,j,sum;
 	printf("Moi ban nhap vao so dong: ");
 	scanf("%d",&m);
 	printf("\nMoi ban nhap vao so cot: ");
 	scanf("%d",&n);
 	
-	for(int i=0;i<m;i++){
+	for(i=0;i<m;i++){
 	  	for(int j=0;j<n;j++){
 	  		printf("Moi ban nhap phan tu arr[%d][%d]: ",i,j);
 	  		scanf("%d",&arr[i][j]);
 		  }
 	}
 	
-	for(int i=0;i<m;i++){
+	for(i=m-1; i>0 ;i--){
 		for(int a=0;a<n-1;a++){
-			for(int j=0;j<n-a-1;j++){
-			if(arr[i][j]>arr[i][j+1]){
+			for(int j=n-1; j>0 ;j--){
+			if(arr[i][j]<arr[i][j]){
 				int temp=arr[i][j];
-				arr[i][j]=arr[i][j+1];
-				arr[i][j+1]=temp;
+				arr[i][j]=arr[i][j];
+				arr[i][j]=temp;
 			}
 		}
 	}
 }
 	
-	for(int i=0;i<m;i++){
-		for(int j=0; j<n;j++){
-			printf("%d \n",arr[i][j+i]);
+	for(i=0,j=n-1 ;i<m, j>=0 ;i++,j--){
+		printf("%d\n",arr[i][j]);
 		}
-		printf("\n");
-	}
 }
--- Session10_Ex9.c
+++ Session10_Ex9.c
@@ -2,35 +2,35 @@
 
 int main(){
 	int arr[100][100];
-	int m,n;
+	int m,n,i,j,sum;
 	printf("Moi ban nhap vao so dong: ");
 	scanf("%d",&m);
 	printf("\nMoi ban nhap vao so cot: ");
 	scanf("%d",&n);
 	
-	for(int i=0;i<m;i++){
+	for(i=0;i<m;i++){
 	  	for(int j=0;j<n;j++){
 	  		printf("Moi ban nhap phan tu arr[%d][%d]: ",i,j);
 	  		scanf("%d",&arr[i][j]);
 		  }
 	}
 	
-	for(int i=0;i<m;i++){
+	for(i=0;i<m;i++){
 		for(int a=0;a<n-1;a++){
 			for(int j=0;j<n-a-1;j++){
-			if(arr[i][j]>arr[i][j+1]){
+			if(arr[i][j]>arr[i+1][j+1]){
 				int temp=arr[i][j];
-				arr[i][j]=arr[i][j+1];
-				arr[i][j+1]=temp;
+				arr[i][j]=arr[i+1][j+1];
+				arr[i+1][j+1]=temp;
 			}
 		}
 	}
 }
 	
-	for(int i=0;i<m;i++){
-		for(int j=0; j<n;j++){
-			printf("%d \n",arr[i][j+i]);
+	for(i=0,j=0 ;i<m, j<n ;i++,j++){
+		printf("%d \n",arr[i][j]);
+			sum+=arr[i][j];
 		}
-		printf("\n");
-	}
+	
+		printf("Tong duong cheo chinh la: %d \n",sum);
 }
